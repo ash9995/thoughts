f0b84ec7d063ec33113fd3d217e1d6408c1e731f
ash9995 <aisharsh@hotmail.com>
2023-12-20 09:24:48 +0300

    Initial Commit

---
+++ thoughts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  thoughts
+//
+//  Created by aisha rashid alshammari  on 07/06/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ thoughts/thoughtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  thoughtsApp.swift
+//  thoughts
+//
+//  Created by aisha rashid alshammari  on 07/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct thoughtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
